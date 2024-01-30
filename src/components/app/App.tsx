63a19950e24db60d508fc4d778d14a30366b38e8
Felipe Rosa <felipemax.suporteti@gmail.com>
2024-01-30 15:52:07 -0300

    chore: cleanup

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import { HomePage, LoginPage, NotFoundPage } from './pages';
+import { HomePage, LoginPage, NotFoundPage } from '../../pages';
+import { ROUTES } from '../../models/enums';
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       <header>Header</header>
       <main data-testid="main-app">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route path={ROUTES.OTHERS} element={<NotFoundPage />} />
         </Routes>
       </main>
     </BrowserRouter>
