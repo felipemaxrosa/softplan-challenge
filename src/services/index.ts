24f147317284ff4cc5e9470cca36aee79cfb59f4
Felipe Rosa <felipemax.suporteti@gmail.com>
2024-02-02 06:08:20 -0300

    chore: cleanup

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,2 @@
-export * as storageService from './storageService';
-export * as userService from './userService';
+export * as storageService from './storage-service';
+export * as userService from './user-service';
--- src/services/user-service.spec.ts
+++ src/services/user-service.spec.ts
@@ -1,6 +1,13 @@
-import * as userService from './userService';
+import { storageService } from '.';
+import { mockUsers } from '../store/mocks';
+import { STORAGE_USERS } from '../constants';
+import * as userService from './user-service';
 
 describe('userService', () => {
+  beforeAll(() => {
+    storageService.setItem(STORAGE_USERS, mockUsers);
+  });
+
   it('should return NULL if user not found', () => {
     const user = { name: 'some', password: '1234' };
     const result = userService.validateCredentials(user);
